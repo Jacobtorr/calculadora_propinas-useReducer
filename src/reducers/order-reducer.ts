8a702ee1a4bfba8413062c1bba77c5318e15afe3
unknown <jator1996@gmail.com>
2024-11-07 12:09:40 -0500

    first commit

---
+++ src/reducers/order-reducer.ts
@@ -0,0 +1,80 @@
+import { MenuItem, OrderItem } from "../types";
+
+// Actions
+ export type OrderActions = 
+ { type: 'add-item', payload: {item: MenuItem} } |
+ { type: 'remove-item', payload: {id: MenuItem['id']} } |
+ { type: 'place-order' } |
+ { type: 'add-tip', payload: {value: number} } 
+
+ // Type
+ export type OrderState = {
+    order: OrderItem[],
+    tip: number
+ }
+
+ // State Inicial
+ export const initialState : OrderState = {
+    order: [],
+    tip: 0
+ }
+
+
+ export const orderReducer = (
+    state: OrderState = initialState,
+    action: OrderActions
+ ) => {
+
+    if(action.type === 'add-item') {
+
+        const itemExist = state.order.find(orderItem => orderItem.id === action.payload.item.id)
+        let updatedOrder : OrderItem[] = []
+        if(itemExist) {
+            updatedOrder = state.order.map( orderItem => orderItem.id === action.payload.item.id ? 
+                {...orderItem, quantity: orderItem.quantity + 1 } : 
+                orderItem
+            )
+        } else {
+            const newItem : OrderItem = {...action.payload.item, quantity: 1}
+            updatedOrder = [...state.order, newItem]
+        }
+
+
+        return {
+            ...state,
+            order: updatedOrder
+        }
+    }
+
+    if(action.type === 'remove-item') {
+
+        const order = state.order.filter( item => item.id !== action.payload.id )
+
+        return {
+            ...state,
+            order
+        }
+    }
+
+    if(action.type === 'place-order') {
+
+        return {
+            ...state,
+            order: [],
+            tip: 0
+        }
+    }
+
+    if(action.type === 'add-tip') {
+
+        const tip = action.payload.value
+
+        return {
+            ...state,
+            tip
+        }
+    }
+
+    return state
+ }
+
